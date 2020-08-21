c3b10666d502b786d74d07c06fd83c3461933941
daglemino <danneydan@gmx.at>
2020-08-21 12:28:19 +0200

    v0.2.5-pre :  improvement code coverage

--- src/main/java/neureka/calculus/environment/operations/function/Ligmoid.java
+++ src/main/java/neureka/calculus/environment/operations/function/Ligmoid.java
@@ -49,42 +49,39 @@ public class Ligmoid extends OperationType
                     return true;
                 }
         ).setADAgentCreator(
-    ( Function f, Tsr derivv, ExecutionCall<Device> call, boolean forward ) ->
-    {
-        Function mul = Function.Detached.MUL;
-        if (
-            derivv != null
-        ) {
-            return new ADAgent(
-                    () -> derivv,
-                    ( t, derivative ) -> mul.call(new Tsr[]{derivative, derivv}),
-                    null
-            );
-        }
-        Tsr[] inputs = call.getTensors();
-        int d = call.getDerivativeIndex();
-        if( forward )
-        {
-            Tsr deriv = f.derive(inputs, d);
-            return new ADAgent(
-                    () -> deriv,
-                    ( t, derivative ) -> mul.call(new Tsr[]{derivative, deriv}),
-                    null
-            );
-        }
-        else
-        {
-
+            ( Function f, Tsr derivv, ExecutionCall<Device> call, boolean forward ) ->
             {
-                Tsr deriv = f.derive(inputs, d);
-                return new ADAgent(
-                        ()->deriv,
-                        (t, derivative) -> mul.call(new Tsr[]{derivative, deriv}),
-                        (t, error) -> mul.call(new Tsr[]{error, deriv})
-                );
+                Function mul = Function.Detached.MUL;
+                if (
+                    derivv != null
+                ) {
+                    return new ADAgent(
+                            () -> derivv,
+                            ( t, derivative ) -> mul.call(new Tsr[]{derivative, derivv}),
+                            null
+                    );
+                }
+                Tsr[] inputs = call.getTensors();
+                int d = call.getDerivativeIndex();
+                if( forward )
+                {
+                    Tsr deriv = f.derive(inputs, d);
+                    return new ADAgent(
+                            () -> deriv,
+                            ( t, derivative ) -> mul.call(new Tsr[]{derivative, deriv}),
+                            null
+                    );
+                }
+                else
+                {
+                        Tsr deriv = f.derive(inputs, d);
+                        return new ADAgent(
+                                ()->deriv,
+                                (t, derivative) -> mul.call(new Tsr[]{derivative, deriv}),
+                                (t, error) -> mul.call(new Tsr[]{error, deriv})
+                        );
+                }
             }
-        }
-    }
         ).setCallHock(
                 ( caller, call ) -> null
         ).setRJAgent(
--- src/test/groovy/ut/calculus/Calculus_Implementation_Unit_Tests.groovy
+++ src/test/groovy/ut/calculus/Calculus_Implementation_Unit_Tests.groovy
@@ -6,10 +6,10 @@ import neureka.autograd.ADAgent
 import neureka.calculus.Function
 import neureka.calculus.environment.ExecutionCall
 import neureka.calculus.environment.OperationTypeImplementation
+import neureka.calculus.environment.implementations.Activation
 import neureka.calculus.environment.implementations.Convolution
 import neureka.calculus.environment.implementations.Operator
 import neureka.calculus.environment.operations.OperationContext
-import spock.lang.Ignore
 import spock.lang.Specification
 
 class Calculus_Implementation_Unit_Tests extends Specification
@@ -67,6 +67,56 @@ class Calculus_Implementation_Unit_Tests extends Specification
     }
 
 
+    def 'Activation implementations behave as expected.'(
+            OperationTypeImplementation imp
+    ){
+
+        given : 'The current Neureka instance is being reset.'
+            Neureka.instance().reset()
+
+        and : 'A mock Function.'
+            def function = Mock(Function)
+            def derivative = Mock(Tsr)
+            function.derive(*_) >> derivative
+
+        and : 'A mock ExecutionCall.'
+            def call = Mock(ExecutionCall)
+
+        when :
+            ADAgent agent = imp.ADAgentCreator.getADAgentOf(
+                function,
+                null,
+                call,
+                true
+            )
+
+        then :
+            agent.isForward()
+            agent.derivative() == derivative
+
+        when :
+            agent = imp.ADAgentCreator.getADAgentOf(
+                function,
+                null,
+                call,
+                false
+            )
+
+        then :
+            !agent.isForward()
+            agent.derivative() == derivative
+
+        where : 'The variable "imp" is from a List of OperationType implementations of activation types.'
+            imp << OperationContext.instance()
+                .instances()
+                .stream()
+                .filter(
+                        e ->
+                                        e.supports( Activation.class )
+                ).map( e -> e.getImplementation( Activation.class ) )
+    }
+
+
     def 'Convolution implementations behave as expected.'(
             OperationTypeImplementation imp
     ){
@@ -106,7 +156,7 @@ class Calculus_Implementation_Unit_Tests extends Specification
             !agent.isForward()
             agent.derivative() == derivative
 
-        where : 'The variable "imp" is from a List of OperationType implementations of operator types.'
+        where : 'The variable "imp" is from a List of OperationType implementations of convolution types.'
             imp << OperationContext.instance()
                 .instances()
                 .stream()
