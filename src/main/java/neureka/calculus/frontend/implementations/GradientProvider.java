deebe62971144c97d239a6622974b0de410c238d
Gleethos <gleethos@gmx.at>
2020-11-12 11:48:04 +0100

    v0.4.0-pre : improve code documentation

--- src/main/java/neureka/calculus/frontend/implementations/GradientProvider.java
+++ src/main/java/neureka/calculus/frontend/implementations/GradientProvider.java
@@ -1,7 +1,47 @@
+/*
+MIT License
+
+Copyright (c) 2019 Gleethos
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*/
+
 package neureka.calculus.frontend.implementations;
 
-public interface GradientProvider {
+/**
+ *  This is a simple interface whose single purpose is
+ *  to serve as a contract for a common method between
+ *  the "FunctionInput" and "FunctionVariable" which
+ *  both ought to posses the ability to return gradients
+ *  if they are configured in such a way.
+ *  The method is also only used internally by these
+ *  two classes.
+ *  There is not other purpose to this interface than that.
+ *  It is not intended for use external to this package.
+ */
+interface GradientProvider
+{
 
+    /**
+     * @return The answer of the question : "does this variable/input node return a gradient?"
+     */
     boolean providesGradient();
 
 }
