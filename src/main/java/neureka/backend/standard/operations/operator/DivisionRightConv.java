bef9c2671b844e8d29a6671e659ab9985ba83b35
Gleethos <danneydan@gmx.at>
2021-05-25 21:41:29 +0200

    further refactor backend

---
+++ src/main/java/neureka/backend/standard/operations/operator/DivisionRightConv.java
@@ -0,0 +1,44 @@
+package neureka.backend.standard.operations.operator;
+
+import neureka.backend.api.operations.AbstractOperation;
+import neureka.backend.api.operations.OperationBuilder;
+import neureka.calculus.Function;
+
+public class DivisionRightConv extends AbstractOperation {
+
+    public DivisionRightConv() {
+        super(
+                new OperationBuilder()
+                        .setFunction(         ""                 )
+                        .setOperator(         "d" + ((char) 187) )
+                        .setArity(            3                  )
+                        .setIsOperator(       true               )
+                        .setIsIndexer(        false              )
+                        .setIsDifferentiable( true               )
+                        .setIsInline(         false              )
+        );
+    }
+
+    @Override
+    public String stringify(String[] children) {
+        StringBuilder reconstructed = new StringBuilder();
+        for ( int i = 0; i < children.length; ++i ) {
+            reconstructed.append( children[ i ] );
+            if ( i < children.length - 1 ) {
+                reconstructed.append(" d" + ((char) 187)+" ");
+            }
+        }
+        return "(" + reconstructed + ")";
+    }
+
+    @Override
+    public String asDerivative(Function[] children, int d ) {
+        throw new IllegalStateException("Operation does not support dynamic derivation!");
+    }
+
+    @Override
+    public double calculate( double[] inputs, int j, int d, Function[] src ) {
+        return src[ 0 ].call( inputs, j );
+    }
+
+}
