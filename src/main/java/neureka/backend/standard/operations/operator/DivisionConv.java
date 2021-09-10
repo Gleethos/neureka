44c6fc84d7992b4cb0ae5bea38c244bdc40c68d8
Gleethos <danneydan@gmx.at>
2021-09-10 16:30:34 +0200

    improve backend api

--- src/main/java/neureka/backend/standard/operations/operator/DivisionConv.java
+++ src/main/java/neureka/backend/standard/operations/operator/DivisionConv.java
@@ -62,7 +62,7 @@ public class DivisionConv extends AbstractOperation {
                                     }
                                 }
                         )
-                        .setHandleInsteadOfDevice( (caller, call) -> CalcUtil.executeFor( caller, call ) )
+                        .setHandleInsteadOfDevice( (caller, call) -> CalcUtil.executeFor( caller, call, (executionCall, executor) -> null ) )
                         .setInstantiateNewTensorsForExecutionIn(
                                 call -> {
                                     Tsr[] tsrs = call.getTensors();
