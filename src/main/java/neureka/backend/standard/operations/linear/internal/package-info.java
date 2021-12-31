1415163ff7abd00182797695617c672cc204738b
Gleethos <danneydan@gmx.at>
2021-12-31 15:41:30 +0100

    document stuff

--- src/main/java/neureka/backend/standard/operations/linear/internal/package-info.java
+++ src/main/java/neureka/backend/standard/operations/linear/internal/package-info.java
@@ -1,5 +1,11 @@
 /**
  * Everything in this package should be considered library-private!
  * DO NOT USE CLASSES INSIDE THIS PACKAGE!
+ *
+ * This package is all about the low level stuff
+ * we need for CPU based operations (on the JVM)...
+ * So there is a lot of primitive array based boilerplate in here
+ * which is designed to allow for the JVMs auto-vectorization to kick in.
+ *
  */
 package neureka.backend.standard.operations.linear.internal;
