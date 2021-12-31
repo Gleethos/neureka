195e2253f1eeb87678bdba5c64dec46f8e14e517
Gleethos <danneydan@gmx.at>
2021-12-31 11:39:36 +0100

    introduce new M64

---
+++ src/main/java/neureka/backend/standard/operations/linear/fast/M64.java
@@ -0,0 +1,47 @@
+package neureka.backend.standard.operations.linear.fast;
+
+import neureka.backend.standard.operations.linear.fast.matrix.operation.Multiply;
+
+public class M64 {
+
+    private final double[] _data;
+    private final int _rowCount, _colCount;
+
+    public M64(
+            int rowCount,
+            int colCount,
+            double[] data
+    ) {
+        _data = data;
+        _rowCount = rowCount; // M
+        _colCount = colCount; // N
+    }
+
+    public M64 multiply( M64 matrix, double[] otherData ) {
+
+        int otherColCount = matrix._colCount;
+
+        M64 retVal = new M64(_rowCount, otherColCount, otherData);
+
+        Multiply
+            .newPrimitive64(_rowCount, otherColCount)
+            .invoke(
+                    retVal._data, _data, _colCount, matrix._data
+            );
+            return retVal;
+    }
+
+    public int getRowDim() {return _rowCount;}
+
+    public int getColDim() {return _colCount;}
+
+    public double[] getData() {
+        return _data;
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName()+"["+_rowCount+"x"+_colCount+"]";
+    }
+
+}
