6633ad5c0682f78a1418ea15a641a9edba3d0e5e
Gleethos <danneydan@gmx.at>
2021-12-31 13:55:39 +0100

    remove unused matrix code

--- src/main/java/neureka/backend/standard/operations/linear/fast/array/operation/OperationUnary.java
+++ src/main/java/neureka/backend/standard/operations/linear/fast/array/operation/OperationUnary.java
@@ -3,8 +3,6 @@ package neureka.backend.standard.operations.linear.fast.array.operation;
 
 import neureka.backend.standard.operations.linear.fast.array.F32Array;
 import neureka.backend.standard.operations.linear.fast.array.F64Array;
-import neureka.backend.standard.operations.linear.fast.function.BinaryFunction.FixedFirst;
-import neureka.backend.standard.operations.linear.fast.function.BinaryFunction.FixedSecond;
 import neureka.backend.standard.operations.linear.fast.function.FunMath;
 import neureka.backend.standard.operations.linear.fast.function.ParameterFunction.FixedParameter;
 import neureka.backend.standard.operations.linear.fast.function.UnaryFunction;
@@ -40,12 +38,6 @@ public final class OperationUnary {
             final UnaryFunction<Double> function) {
         if (function == FunMath.NEGATE) {
             PrimitiveArrayOperation.negate(data, first, limit, step, values);
-        } else if (function instanceof FixedFirst<?>) {
-            final FixedFirst<Double> tmpFunc = (FixedFirst<Double>) function;
-            OperationBinary.invoke(data, first, limit, step, tmpFunc.floatValue(), tmpFunc.getFunction(), values);
-        } else if (function instanceof FixedSecond<?>) {
-            final FixedSecond<Double> tmpFunc = (FixedSecond<Double>) function;
-            OperationBinary.invoke(data, first, limit, step, values, tmpFunc.getFunction(), tmpFunc.floatValue());
         } else if (function instanceof FixedParameter<?>) {
             final FixedParameter<Double> tmpFunc = (FixedParameter<Double>) function;
             OperationParameter.invoke(data, first, limit, step, values, tmpFunc.getFunction(), tmpFunc.getParameter());
@@ -66,12 +58,6 @@ public final class OperationUnary {
     static void invoke(final double[] data, final int first, final int limit, final int step, final double[] values, final UnaryFunction<Double> function) {
         if (function == FunMath.NEGATE) {
             PrimitiveArrayOperation.negate(data, first, limit, step, values);
-        } else if (function instanceof FixedFirst<?>) {
-            final FixedFirst<Double> tmpFunc = (FixedFirst<Double>) function;
-            OperationBinary.invoke(data, first, limit, step, tmpFunc.doubleValue(), tmpFunc.getFunction(), values);
-        } else if (function instanceof FixedSecond<?>) {
-            final FixedSecond<Double> tmpFunc = (FixedSecond<Double>) function;
-            OperationBinary.invoke(data, first, limit, step, values, tmpFunc.getFunction(), tmpFunc.doubleValue());
         } else if (function instanceof FixedParameter<?>) {
             final FixedParameter<Double> tmpFunc = (FixedParameter<Double>) function;
             OperationParameter.invoke(data, first, limit, step, values, tmpFunc.getFunction(), tmpFunc.getParameter());
