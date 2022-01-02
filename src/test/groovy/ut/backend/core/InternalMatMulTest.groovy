8349f8d6e5c373ee837dfc2efbb0a0b636cc4c80
Gleethos <danneydan@gmx.at>
2022-01-02 12:48:47 +0100

    simplify testing

--- src/test/groovy/ut/backend/core/InternalMatMulTest.groovy
+++ src/test/groovy/ut/backend/core/InternalMatMulTest.groovy
@@ -29,70 +29,68 @@ class InternalMatMulTest {
 
     static void start() {
 
-        //---
+        Type.COL_MAJOR.set()
+        _test(11,7,13,-1572171092)
 
+        Type.COL_MAJOR.set()
+        _test(2,2,2,1621312579)
+        _test(3,3,3,458449078)
+        _test(4,4,4,-1394046135)
+        _test(5,5,5,726435157)
+        _test(6,6,6,952800666)
+        _test(7,7,7,478609247)
+        _test(8,8,8,-1274930005)
+        _test(9,9,9,1754156081)
+        _test(10,10,10,1703295132)
+
+        _test(1,1,10,-1194484459)
+        _test(1,1,9,-242625105)
+        _test(1,1,8,-1060007964)
+        _test(1,1,7,1047354419)
+        _test(1,1,6,606279625)
+        _test(1,1,5,800495719)
+        _test(1,1,4,-605917383)
+        _test(1,1,3,1703409186)
+        _test(1,1,2,-1782294977)
+
+        _test(1,1,1,-1641913795)
+
+        _test(2,1,1,-1782294977)
+        _test(3,1,1,1703409186)
+        _test(4,1,1,-605917383)
+        _test(5,1,1,800495719)
+        _test(6,1,1,606279625)
+        _test(7,1,1,1047354419)
+        _test(8,1,1,-1060007964)
+        _test(9,1,1,-242625105)
+        _test(10,1,1,-1194484459)
+        _test(1,2,1,-1639292187)
+        _test(1,3,1,-1638189920)
+        _test(1,4,1,-1637296190)
+        _test(1,5,1,-1637177026)
+        _test(1,6,1,-1636521624)
+        _test(1,7,1,-1636521624)
+        _test(1,8,1,-1636491833)
+        _test(1,9,1,-1636372669)
+        _test(1,10,1,-1635478939)
+        _test(DIM,DIM,DIM,245517809)
+
+        //---
         Type.ROW_MAJOR.set()
         _testFloats(2,3,4, -424206796)
         _testFloats(20,30,40, -874284717)
         _testFloats(42,61,53, -37310652)
 
         //---
-
         _basicF64Test(Type.ROW_MAJOR)
         _basicF64Test(Type.COL_MAJOR)
         _basicF32Test(Type.ROW_MAJOR)
         _basicF32Test(Type.COL_MAJOR)
-
-        _test(11,7,13,-1572171092,400854770)
-
-        Type.COL_MAJOR.set()
-        _test(2,2,2,1621312579,-1622243452)
-        _test(3,3,3,458449078,175514624)
-        _test(4,4,4,-1394046135,1168903786)
-        _test(5,5,5,726435157,1348725681)
-        _test(6,6,6,952800666,-1151626979)
-        _test(7,7,7,478609247,202982307)
-        _test(8,8,8,-1274930005,1281513236)
-        _test(9,9,9,1754156081,-1504275778)
-        _test(10,10,10,1703295132,-1973150495)
-
-        _test(1,1,10,-1194484459,155274511)
-        _test(1,1,9,-242625105,72695794)
-        _test(1,1,8,-1060007964,-1265014008)
-        _test(1,1,7,1047354419,-1000173898)
-        _test(1,1,6,606279625,-138392009)
-        _test(1,1,5,800495719,1248567540)
-        _test(1,1,4,-605917383,93003368)
-        _test(1,1,3,1703409186,-1524639888)
-        _test(1,1,2,-1782294977,853895268)
-
-        _test(1,1,1,-1641913795,-1642807525)
-
-        _test(2,1,1,-1782294977,853895268)
-        _test(3,1,1,1703409186,-1524639888)
-        _test(4,1,1,-605917383,93003368)
-        _test(5,1,1,800495719,1248567540)
-        _test(6,1,1,606279625,-138392009)
-        _test(7,1,1,1047354419,-1000173898)
-        _test(8,1,1,-1060007964,-1265014008)
-        _test(9,1,1,-242625105,72695794)
-        _test(10,1,1,-1194484459,155274511)
-        _test(1,2,1,-1639292187,-1641913795)
-        _test(1,3,1,-1638189920,-1641794631)
-        _test(1,4,1,-1637296190,-1641139229)
-        _test(1,5,1,-1637177026,-1641139229)
-        _test(1,6,1,-1636521624,-1641109438)
-        _test(1,7,1,-1636521624,-1640990274)
-        _test(1,8,1,-1636491833,-1640096544)
-        _test(1,9,1,-1636372669,-1638368666)
-        _test(1,10,1,-1635478939,-1636372669)
-        _test(DIM,DIM,DIM,245517809,1204091031)
-
     }
 
-    private static void _test(int d1, int d2, int d3, int hash1, int hash2) {
+    private static void _test(int d1, int d2, int d3, int hash1) {
         _testDoubles(d1,d2,d3,hash1)
-        _testFloats(d1,d2,d3,hash2)
+        _testFloats2(d1,d2,d3,hash1)
     }
 
     private static void _testDoubles(int dim, int dim2, int dim3, int hash) {
@@ -106,6 +104,19 @@ class InternalMatMulTest {
         var C = _matmulF64(A,B)
 
         assert _hash(C.getData()) ==  hash
+    }
+
+    private static void _testFloats2(int dim, int dim2, int dim3, int hash) {
+
+        var A = new M32(dim, dim2, new float[dim*dim2])
+        var B = new M32(dim2, dim3, new float[dim2*dim3])
+
+        _fillIt32(A.data, 43)
+        _fillIt32(B.data, 87)
+
+        var C = _matmulF32(A,B)
+
+        assert _hash(C.getData()) == hash
     }
 
     private static void _testFloats(int dim, int dim2, int dim3, int hash) {
@@ -221,8 +232,10 @@ class InternalMatMulTest {
     private static int _hash(Object data) {
         if ( data instanceof float[] )
             return Arrays.toString((float[])data).hashCode()
-        else
+        else if ( data instanceof double[] )
             return Arrays.toString((double[])data).hashCode()
+        else
+            return data.hashCode()
     }
 
     private static void _fillIt64( double[] data, int seed ) {
