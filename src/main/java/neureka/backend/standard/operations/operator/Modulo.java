84af12fa638744c960618cf67efcddd63edcac87
Gleethos <danneydan@gmx.at>
2022-01-10 12:32:38 +0100

    improve backend code quality

--- src/main/java/neureka/backend/standard/operations/operator/Modulo.java
+++ src/main/java/neureka/backend/standard/operations/operator/Modulo.java
@@ -98,26 +98,6 @@ public class Modulo extends AbstractOperation {
         //________________
         // BROADCASTING :
 
-        DefaultOperatorCreator<TertiaryF64NDFun> creator =
-                ( inputs, d ) -> {
-                    double[] t1_val = inputs[ 1 ].getDataAs( double[].class );
-                    double[] t2_val = inputs[ 2 ].getDataAs( double[].class );
-                    if ( d < 0 ) {
-                        return ( t0Idx, t1Idx, t2Idx ) -> t1_val[ t1Idx.i() ] % t2_val[t2Idx.i()];
-                    } else {
-                        return ( t0Idx, t1Idx, t2Idx ) -> {
-                            if ( d == 0 ) {
-                                return 1 / t2_val[t2Idx.i()];
-                            } else {
-                                return
-                                        -(t1_val[ t1Idx.i() ]
-                                                /
-                                        Math.pow(t2_val[t2Idx.i()], 2));
-                            }
-                        };
-                    }
-                };
-
         Broadcast broadcast = new Broadcast((executionCall, executor) -> null)
             .setCanPerformBackwardADFor( call -> true )
             .setCanPerformForwardADFor(
@@ -161,17 +141,13 @@ public class Modulo extends AbstractOperation {
                         CPUImplementation
                             .withArity(3)
                             .andImplementation(
-                                call ->
-                                        call.getDevice().getExecutor()
-                                                .threaded(
-                                                        call.getTsrOfType( Number.class, 0 ).size(),
-                                                        ( start, end ) ->
-                                                                Broadcast.broadcast (
-                                                                        call.getTsrOfType( Number.class, 0 ), call.getTsrOfType( Number.class, 1 ), call.getTsrOfType( Number.class, 2 ),
-                                                                        call.getValOf( Arg.DerivIdx.class ), start, end,
-                                                                        creator.create(call.getTensors(), call.getValOf( Arg.DerivIdx.class ))
-                                                                )
-                                                )
+                                    Broadcast.implementationForCPU()
+                                            .with(Fun.F64F64ToF64.triple(
+                                                    ( a, b ) -> a % b,
+                                                    ( a, b ) -> 1 / b, // Deriving at input 0
+                                                    ( a, b ) -> -(a / Math.pow(b, 2)) // deriving input 1
+                                            ))
+                                            .get()
                             )
                 )
                 .setImplementationFor(
