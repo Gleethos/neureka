d37374ce9d09a4d45872f7d6cbe2fdb3f4a21a0d
Gleethos <Gleethos@gmx.at>
2022-02-18 18:32:06 +0100

    improve test

--- src/test/groovy/it/device/OpenCLDevice_Integration_Spec.groovy
+++ src/test/groovy/it/device/OpenCLDevice_Integration_Spec.groovy
@@ -366,37 +366,35 @@ class OpenCLDevice_Integration_Spec extends Specification
         when :
             device.compileAdHocKernel( kernelName, """
     kernel void backend_mm_${M}x${K}x${N}(
-        global float* pC,
-        global const float* pA,
-        global const float* pB,
-        int M, int N, int P
+        global float* C,
+        global const float* A,
+        global const float* B,
+        int M, int N, int K
     ){
-        local float shA[16][16];
-        local float shB[16][16];
+        local float locA[16][16];
+        local float locB[16][16];
 
         int m = get_global_id(0);
-        int p = get_global_id(1);
+        int k = get_global_id(1);
 
-        int pc = ( get_group_id(1) << 4 ) + get_local_id(0);
+        int c = ( get_group_id(1) << 4 ) + get_local_id(0);
 
         float result = 0.0;
 
         for ( int n = get_local_id(1); n < N; n += 16 )
         {
-            shA[ get_local_id(0) ][ get_local_id(1) ] = pA[ ( N * m ) + n ];
-            shB[ get_local_id(0) ][ get_local_id(1) ] = pB[ ( P * n ) + pc ];
+            locA[ get_local_id(0) ][ get_local_id(1) ] = A[ ( N * m ) + n ];
+            locB[ get_local_id(0) ][ get_local_id(1) ] = B[ ( K * n ) + c ];
 
             barrier(CLK_LOCAL_MEM_FENCE);
 
             for ( int i = 0; i < 16; i++ )
-            {
-                result += ( shA[ get_local_id(0) ][i] * shB[ get_local_id(1) ][i] );
-            }
-
+                result += ( locA[ get_local_id(0) ][i] * locB[ get_local_id(1) ][i] );
+           
             barrier(CLK_LOCAL_MEM_FENCE);
         }
 
-        pC[ ( P * m ) + p ] = result;
+        C[ ( K * m ) + k ] = result;
     }
                     """
         )
