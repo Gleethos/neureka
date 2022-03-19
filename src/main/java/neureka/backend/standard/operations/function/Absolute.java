16c3600b2a7cc3da8e7ab6ae1aa344bc7f73cf4c
Gleethos <Gleethos@gmx.at>
2022-03-19 14:34:46 +0100

    improve code reuse

--- src/main/java/neureka/backend/standard/operations/function/Absolute.java
+++ src/main/java/neureka/backend/standard/operations/function/Absolute.java
@@ -1,83 +1,29 @@
 package neureka.backend.standard.operations.function;
 
-import neureka.backend.api.operations.AbstractOperation;
-import neureka.backend.api.operations.OperationBuilder;
-import neureka.backend.standard.algorithms.Activation;
-import neureka.backend.standard.algorithms.internal.Fun;
 import neureka.calculus.Function;
-import neureka.devices.host.CPU;
-import neureka.devices.opencl.OpenCLDevice;
-import org.jetbrains.annotations.Contract;
 
-public final class Absolute extends AbstractOperation
+public final class Absolute extends AbstractActivationOperation
 {
-    public Absolute()
-    {
-        super(
-                new OperationBuilder()
-                .setIdentifier(         "abs"   )
-                .setOperator(         "abs"   )
-                .setArity(            1       )
-                .setIsOperator(       false   )
-                .setIsIndexer(        false   )
-                .setIsDifferentiable( true    )
-                .setIsInline(         false   )
-        );
-        setAlgorithm(
-            new Activation()
-            .setSupplyADAgentFor( getDefaultAlgorithm() )
-            .buildFunAlgorithm()
-            .setImplementationFor(
-                CPU.class,
-                Activation.implementationForCPU()
-                    .with(Fun.F64ToF64.pair(
-                        x -> Math.abs( x ),
-                        x -> ( x < 0 ? -1 : 1 )
-                    ))
-                    .with(Fun.F32ToF32.pair(
-                            x -> Math.abs( x ),
-                            x -> ( x < 0 ? -1 : 1 )
-                    ))
-                    .with(Fun.I32ToI32.pair(
-                            x -> Math.abs( x ),
-                            x -> ( x < 0 ? -1 : 1 )
-                    ))
-                    .get()
-            )
-            .setImplementationFor(
-                OpenCLDevice.class,
-                Activation.implementationForGPU( this.getIdentifier() )
-                        .with( "output = fabs( input );\n" )
-                        .and( "output = ( input < 0 ) ? -1 : 1;\n" )
-            )
-        );
-    }
-
-    @Override
-    public String stringify( String[] children ) {
-        String expression = String.join( ", ", children );
-        if ( expression.startsWith("(") && expression.endsWith(")") ) return getIdentifier() + expression;
-        return getIdentifier() + "(" + expression + ")";
-    }
+    public Absolute() { super( "abs" ); }
 
     @Override
     public String asDerivative( Function[] children, int derivationIndex) {
         throw new IllegalStateException("Operation does not support dynamic derivation!");
     }
 
-    @Override
-    public double calculate( double[] inputs, int j, int d, Function[] src ) {
-        return calculate(
-                src[ 0 ].call( inputs, j ),
-                d >= 0
-        ) * ( ( d < 0 ) ? 1 : src[ 0 ].derive( inputs, d, j ) );
-    }
+    @Override protected String _activationCode() { return "output = fabs( input );\n"; }
 
-    @Contract( pure = true )
-    public static double calculate( double input, boolean derive ) {
-        if ( !derive ) return Math.abs( input );
-        else return ( input < 0 ) ? -1 : 1;
-    }
+    @Override protected String _derivationCode() { return "output = ( input < 0 ) ? -1 : 1;\n"; }
 
+    @Override protected double _activate(double x) { return Math.abs( x ); }
 
+    @Override protected double _derive(double x) { return ( x < 0d ? -1d : 1d ); }
+
+    @Override protected float _activate(float x) { return Math.abs( x ); }
+
+    @Override protected float _derive(float x) { return ( x < 0f ? -1f : 1f ); }
+
+    @Override protected int _activate(int x) { return Math.abs( x ); }
+
+    @Override protected int _derive(int x) { return ( x < 0 ? -1 : 1 ); }
 }
