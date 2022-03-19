bbe4f7fc54a1178615afb46201d79d5f41a0f5d4
Gleethos <Gleethos@gmx.at>
2022-03-19 14:42:41 +0100

    improve code reuse

--- src/main/java/neureka/backend/standard/operations/function/Gaussian.java
+++ src/main/java/neureka/backend/standard/operations/function/Gaussian.java
@@ -1,63 +1,12 @@
 package neureka.backend.standard.operations.function;
 
-import neureka.backend.api.operations.AbstractOperation;
-import neureka.backend.api.operations.OperationBuilder;
-import neureka.backend.standard.algorithms.Activation;
-import neureka.backend.standard.algorithms.internal.Fun;
 import neureka.calculus.Function;
-import neureka.devices.host.CPU;
-import neureka.devices.opencl.OpenCLDevice;
-import org.jetbrains.annotations.Contract;
 
-public final class Gaussian extends AbstractOperation
+public final class Gaussian extends AbstractActivationOperation
 {
     public Gaussian()
     {
-        super(
-            new OperationBuilder()
-                    .setIdentifier(         "gaus"    )
-                    .setOperator(         "gaus"    )
-                    .setArity(            1         )
-                    .setIsOperator(       false     )
-                    .setIsIndexer(        false     )
-                    .setIsDifferentiable( true      )
-                    .setIsInline(         false     )
-        );
-        setAlgorithm(
-            new Activation()
-            .setSupplyADAgentFor( getDefaultAlgorithm() )
-            .buildFunAlgorithm()
-            .setImplementationFor(
-                CPU.class,
-                Activation.implementationForCPU()
-                    .with(Fun.F64ToF64.pair(
-                        x -> Math.exp( -( x * x ) ),
-                        x -> -2 * x * Math.exp( -( x * x ) )
-                    ))
-                    .with(Fun.F32ToF32.pair(
-                        x -> (float) Math.exp( -( x * x ) ),
-                        x -> (float) ( -2 * x * Math.exp( -( x * x ) ) )
-                    ))
-                    .with(Fun.I32ToI32.pair(
-                        x -> (int) Math.round( Math.exp( -( x * x ) ) ),
-                        x -> (int) Math.round( -2 * x * Math.exp( -( x * x ) ) )
-                    ))
-                    .get()
-            )
-            .setImplementationFor(
-                OpenCLDevice.class,
-                    Activation.implementationForGPU( this.getIdentifier() )
-                        .with("output = exp( -( input * input ) );\n" )
-                        .and("output = -2 * input * exp( -( input * input ) );\n")
-            )
-        );
-    }
-
-    @Override
-    public String stringify( String[] children ) {
-        String expression = String.join( ", ", children );
-        if ( expression.startsWith("(") && expression.endsWith(")") ) return getIdentifier() + expression;
-        return getIdentifier() + "(" + expression + ")";
+        super( "gaus" );
     }
 
     @Override
@@ -66,19 +15,23 @@ public final class Gaussian extends AbstractOperation
     }
 
     @Override
-    public double calculate( double[] inputs, int j, int d, Function[] src ) {
-        return calculate(
-                src[ 0 ].call( inputs, j ),
-                d >= 0
-        ) * ( ( d < 0 ) ? 1 : src[ 0 ].derive( inputs, d, j ) );
+    protected String _activationCode() {
+        return "output = exp( -( input * input ) );\n";
     }
 
-    @Contract(pure = true)
-    public static double calculate(double input, boolean derive ) {
-        if ( !derive ) return Math.pow(Math.E, -Math.pow(input, 2));
-        else return -2 * input * Math.pow(Math.E, -Math.pow(input, 2));
+    @Override
+    protected String _derivationCode() {
+        return "output = -2 * input * exp( -( input * input ) );\n";
     }
 
+    @Override
+    protected double _activate(double x) {
+        return Math.exp( -( x * x ) );
+    }
 
+    @Override
+    protected double _derive(double x) {
+        return -2 * x * Math.exp( -( x * x ) );
+    }
 
 }
--- src/main/java/neureka/backend/standard/operations/function/Tanh.java
+++ src/main/java/neureka/backend/standard/operations/function/Tanh.java
@@ -1,11 +1,6 @@
 package neureka.backend.standard.operations.function;
 
-import neureka.backend.api.operations.OperationBuilder;
-import neureka.backend.standard.algorithms.Activation;
-import neureka.backend.standard.algorithms.internal.Fun;
 import neureka.calculus.Function;
-import neureka.devices.host.CPU;
-import neureka.devices.opencl.OpenCLDevice;
 
 public final class Tanh extends AbstractActivationOperation
 {
