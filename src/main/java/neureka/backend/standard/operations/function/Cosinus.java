f234bfc4419a8aebc5d5826d32acc04124953d5d
Gleethos <Gleethos@gmx.at>
2022-03-19 14:09:54 +0100

    improve code reuse

--- src/main/java/neureka/backend/standard/operations/function/Cosinus.java
+++ src/main/java/neureka/backend/standard/operations/function/Cosinus.java
@@ -1,64 +1,10 @@
 package neureka.backend.standard.operations.function;
 
-import neureka.backend.api.operations.AbstractOperation;
-import neureka.backend.api.operations.OperationBuilder;
-import neureka.backend.standard.algorithms.Activation;
-import neureka.backend.standard.algorithms.internal.Fun;
 import neureka.calculus.Function;
-import neureka.devices.host.CPU;
-import neureka.devices.opencl.OpenCLDevice;
-import org.jetbrains.annotations.Contract;
 
-public final class Cosinus extends AbstractOperation
+public final class Cosinus extends AbstractActivationOperation
 {
-    public Cosinus()
-    {
-        super (
-            new OperationBuilder()
-                .setIdentifier(         "cos"  )
-                .setOperator(         "cos"  )
-                .setArity(            1      )
-                .setIsOperator(       false  )
-                .setIsIndexer(        false  )
-                .setIsDifferentiable( true   )
-                .setIsInline(         false  )
-        );
-        setAlgorithm(
-            new Activation()
-                .setSupplyADAgentFor( getDefaultAlgorithm() )
-                .buildFunAlgorithm()
-                .setImplementationFor(
-                    CPU.class,
-                    Activation.implementationForCPU()
-                        .with(Fun.F64ToF64.pair(
-                            x -> Math.cos(x),
-                            x -> -Math.sin(x)
-                        ))
-                        .with(Fun.F32ToF32.pair(
-                            x -> (float) Math.cos(x),
-                            x -> (float) -Math.sin(x)
-                        ))
-                        .with(Fun.I32ToI32.pair(
-                            x -> (int) Math.cos(x),
-                            x -> (int) -Math.sin(x)
-                        ))
-                        .get()
-                )
-                .setImplementationFor(
-                    OpenCLDevice.class,
-                    Activation.implementationForGPU( this.getIdentifier() )
-                            .with( "output = cos( input );\n" )
-                            .and( "output = -sin( input );\n" )
-                )
-            );
-    }
-
-    @Override
-    public String stringify( String[] children ) {
-        String expression = String.join( ", ", children );
-        if ( expression.startsWith("(") && expression.endsWith(")") ) return getIdentifier() + expression;
-        return getIdentifier() + "(" + expression + ")";
-    }
+    public Cosinus() { super( "cos" ); }
 
     @Override
     public String asDerivative( Function[] children, int derivationIndex) {
@@ -66,18 +12,15 @@ public final class Cosinus extends AbstractOperation
     }
 
     @Override
-    public double calculate( double[] inputs, int j, int d, Function[] src ) {
-        return calculate(
-                src[ 0 ].call( inputs, j ),
-                d >= 0
-        ) * ( ( d < 0 ) ? 1 : src[ 0 ].derive( inputs, d, j ) );
-    }
+    protected String _activationCode() { return "output = cos( input );\n"; }
 
-    @Contract(pure = true)
-    public static double calculate( double input, boolean derive ) {
-        if ( !derive ) return Math.cos( input );
-        else return -Math.sin( input );
-    }
+    @Override
+    protected String _derivationCode() { return "output = -sin( input );\n"; }
 
+    @Override
+    protected double _activate(double x) { return Math.cos(x); }
+
+    @Override
+    protected double _derive(double x) { return -Math.sin(x); }
 
 }
