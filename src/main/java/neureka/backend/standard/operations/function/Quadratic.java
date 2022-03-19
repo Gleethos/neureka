058424ce25b3b92ac9d300d1152720215f5f2877
Gleethos <Gleethos@gmx.at>
2022-03-19 14:25:17 +0100

    improve code reuse

--- src/main/java/neureka/backend/standard/operations/function/Quadratic.java
+++ src/main/java/neureka/backend/standard/operations/function/Quadratic.java
@@ -1,82 +1,30 @@
 package neureka.backend.standard.operations.function;
 
-import neureka.backend.api.operations.AbstractOperation;
-import neureka.backend.api.operations.OperationBuilder;
-import neureka.backend.standard.algorithms.Activation;
-import neureka.backend.standard.algorithms.internal.Fun;
 import neureka.calculus.Function;
-import neureka.devices.host.CPU;
-import neureka.devices.opencl.OpenCLDevice;
-import org.jetbrains.annotations.Contract;
 
-public final class Quadratic extends AbstractOperation
+public final class Quadratic extends AbstractActivationOperation
 {
-    public Quadratic() {
-        super(
-            new OperationBuilder()
-                    .setIdentifier(         "quad"  )
-                    .setOperator(         "quad"  )
-                    .setArity(            1      )
-                    .setIsOperator(       false  )
-                    .setIsIndexer(        false  )
-                    .setIsDifferentiable( true   )
-                    .setIsInline(         false  )
-        );
-        setAlgorithm(
-            new Activation()
-            .setSupplyADAgentFor( getDefaultAlgorithm() )
-            .buildFunAlgorithm()
-            .setImplementationFor(
-                CPU.class,
-                Activation.implementationForCPU()
-                    .with(Fun.F64ToF64.pair(
-                        x -> x * x,
-                        x -> 2 * x
-                    ))
-                    .with(Fun.F32ToF32.pair(
-                        x -> x * x,
-                        x -> 2 * x
-                    ))
-                    .with(Fun.I32ToI32.pair(
-                        x -> x * x,
-                        x -> 2 * x
-                    ))
-                    .get()
-            )
-            .setImplementationFor(
-                OpenCLDevice.class,
-                    Activation.implementationForGPU( this.getIdentifier() )
-                            .with( "output = input * input;\n" )
-                            .and( "output = 2 * input;\n" )
-            )
-        );
-    }
-
-    @Override
-    public String stringify( String[] children ) {
-        String expression = String.join( ", ", children );
-        if ( expression.startsWith("(") && expression.endsWith(")") ) return getIdentifier() + expression;
-        return getIdentifier() + "(" + expression + ")";
-    }
+    public Quadratic() { super( "quad" ); }
 
     @Override
     public String asDerivative( Function[] children, int derivationIndex) {
         throw new IllegalStateException("Operation does not support dynamic derivation!");
     }
 
-    @Override
-    public double calculate( double[] inputs, int j, int d, Function[] src ) {
-        return calculate(
-                src[ 0 ].call( inputs, j ),
-                d >= 0
-        ) * ( ( d < 0 ) ? 1 : src[ 0 ].derive( inputs, d, j ) );
-    }
+    @Override protected String _activationCode() { return "output = input * input;\n"; }
 
-    @Contract(pure = true)
-    public static double calculate(double input, boolean derive ) {
-        if (!derive) return (input * input);
-        else return 2 * input;
-    }
+    @Override protected String _derivationCode() { return "output = 2 * input;\n"; }
 
+    @Override protected double _activate(double x) { return x * x; }
+
+    @Override protected double _derive(double x) { return 2 * x; }
+
+    @Override protected float _activate(float x) { return x * x; }
+
+    @Override protected float _derive(float x) { return 2 * x; }
+
+    @Override protected int _activate(int x) { return x * x; }
+
+    @Override protected int _derive(int x) { return 2 * x; }
 
 }
