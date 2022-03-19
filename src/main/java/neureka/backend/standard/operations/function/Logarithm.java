375525a05304084f7a3ca844910c6a0b33e6a491
Gleethos <Gleethos@gmx.at>
2022-03-19 14:31:05 +0100

    improve code reuse

--- src/main/java/neureka/backend/standard/operations/function/Logarithm.java
+++ src/main/java/neureka/backend/standard/operations/function/Logarithm.java
@@ -1,65 +1,10 @@
 package neureka.backend.standard.operations.function;
 
-import neureka.backend.api.operations.AbstractOperation;
-import neureka.backend.api.operations.OperationBuilder;
-import neureka.backend.standard.algorithms.Activation;
-import neureka.backend.standard.algorithms.internal.Fun;
 import neureka.calculus.Function;
-import neureka.devices.host.CPU;
-import neureka.devices.opencl.OpenCLDevice;
-import org.jetbrains.annotations.Contract;
 
-public final class Logarithm extends AbstractOperation
+public final class Logarithm extends AbstractActivationOperation
 {
-    public Logarithm()
-    {
-        super (
-            new OperationBuilder()
-                    .setIdentifier(         "ln"  )
-                    .setOperator(         "ln"  )
-                    .setArity(            1      )
-                    .setIsOperator(       false  )
-                    .setIsIndexer(        false  )
-                    .setIsDifferentiable( true   )
-                    .setIsInline(         false  )
-        );
-        setAlgorithm(
-            new Activation()
-            .setSupplyADAgentFor( getDefaultAlgorithm() )
-            .buildFunAlgorithm()
-            .setImplementationFor(
-                CPU.class,
-                Activation.implementationForCPU()
-                    .with(Fun.F64ToF64.pair(
-                        x -> Math.log(x),
-                        x -> 1d / x
-                    ))
-                    .with(Fun.F32ToF32.pair(
-                        x -> (float) Math.log(x),
-                        x -> 1f / x
-                    ))
-                    .with(Fun.I32ToI32.pair(
-                            x -> (int) Math.round(Math.log(x)),
-                            x -> (int) Math.round( 1d / x )
-                    ))
-                    .get()
-            )
-            .setImplementationFor(
-                OpenCLDevice.class,
-                    Activation.implementationForGPU( this.getIdentifier() )
-                            .with( "output = log( input );\n" )
-                            .and( "output = 1.0 / ( input );\n" )
-            )
-        );
-
-    }
-
-    @Override
-    public String stringify( String[] children ) {
-        String expression = String.join( ", ", children );
-        if ( expression.startsWith("(") && expression.endsWith(")") ) return getIdentifier() + expression;
-        return getIdentifier() + "(" + expression + ")";
-    }
+    public Logarithm() { super( "ln" ); }
 
     @Override
     public String asDerivative( Function[] children, int derivationIndex) {
@@ -68,18 +13,15 @@ public final class Logarithm extends AbstractOperation
     }
 
     @Override
-    public double calculate( double[] inputs, int j, int d, Function[] src ) {
-        return calculate(
-                src[ 0 ].call( inputs, j ),
-                d >= 0
-        ) * ( ( d < 0 ) ? 1 : src[ 0 ].derive( inputs, d, j ) );
-    }
+    protected String _activationCode() { return "output = log( input );\n"; }
 
-    @Contract(pure = true)
-    public static double calculate( double input, boolean derive ) {
-        if ( !derive ) return Math.log( input );
-        else return 1.0 / input;
-    }
+    @Override
+    protected String _derivationCode() { return "output = 1.0 / ( input );\n"; }
 
+    @Override protected double _activate(double x) { return Math.log(x); }
+
+    @Override protected double _derive(double x) { return 1d / x; }
+
+    @Override protected float _derive(float x) { return 1f / x; }
 
 }
