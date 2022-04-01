eaabddaed54c5b379561abc58e6d1831b61a8552
Gleethos <Gleethos@gmx.at>
2022-04-02 00:09:43 +0200

    improve device IO testing

---
+++ src/test/groovy/ut/device/Cross_Device_IO_Spec.groovy
@@ -0,0 +1,48 @@
+package ut.device
+
+import neureka.Neureka
+import neureka.Tsr
+import neureka.devices.Device
+import neureka.dtype.DataType
+import spock.lang.IgnoreIf
+import spock.lang.Specification
+
+class Cross_Device_IO_Spec extends Specification
+{
+    @IgnoreIf({ !Neureka.get().canAccessOpenCL() && deviceType == 'GPU' })
+    def 'We can use the access device API to read from a tensor.'(
+            String deviceType, Class<Object> type, Object[] fill, Object expected
+    ) {
+        given :
+            var device = Device.find(deviceType)
+            var arrayType = DataType.of(type).dataArrayType()
+        and :
+            var t = Tsr.of(type).withShape(4).andFill(fill).to(device)
+        and :
+            var s = t[1..2]
+
+        expect :
+            device.access(t).readAll(false) == expected
+            device.access(s).readAll(false) == expected
+            device.access(t).readAll(true)  == expected
+            device.access(s).readAll(true)  == expected
+        and :
+            device.access(t).readArray(arrayType, 1, 1) == [expected[1]]
+            device.access(s).readArray(arrayType, 1, 1) == [expected[1]]
+            device.access(t).readArray(arrayType, 1, 1) == [expected[1]]
+            device.access(s).readArray(arrayType, 1, 1) == [expected[1]]
+
+        where :
+            deviceType | type     | fill     |  expected
+            'CPU'      | Integer  | [2, 1]   | [2, 1, 2, 1]
+            'CPU'      | Short    | [2,7,8]  | [2,7,8,2]
+            'CPU'      | Long     | [6,2,6]  | [6,2,6,6]
+            'CPU'      | Byte     | [6,2,7]  | [6,2,7,6]
+            'CPU'      | Double   | [3.4, 3] | [3.4d, 3.0d, 3.4d, 3.0d]
+            'CPU'      | Float    | [5.7,-1] | [5.7f, -1.0f, 5.7f, -1.0f]
+            'GPU'      | Float    | [5.7,-1] | [5.7f, -1.0f, 5.7f, -1.0f]
+    }
+
+
+
+}
