1a4f0de80a4db2c23de0c0eef172185cfabd80bd
Gleethos <Gleethos@gmx.at>
2022-04-23 23:44:59 +0200

    improve new API documentation

--- src/main/java/neureka/backend/api/algorithms/fun/ADSupportPredicate.java
+++ src/main/java/neureka/backend/api/algorithms/fun/ADSupportPredicate.java
@@ -5,12 +5,18 @@ import neureka.devices.Device;
 
 
 /**
- *  A {@link ADSupportPredicate} lambda checks if which auto differentiation mode
+ *  A {@link ADSupportPredicate} lambda checks which auto differentiation mode
  *  can be performed for a given {@link ExecutionCall}.
  *  The analyzer returns a {@link AutoDiff} enum instance.
  */
 public interface ADSupportPredicate {
 
+    /**
+     *  Implementations of this ought to check which auto differentiation mode
+     *  can be performed for a given {@link ExecutionCall}.
+     * @param call The {@link ExecutionCall} which should be checked.
+     * @return A {@link AutoDiff} enum instance describing what kind of differentiation can be performed.
+     */
     AutoDiff autoDiffModeFrom(ExecutionCall<? extends Device<?>> call );
 
 }
