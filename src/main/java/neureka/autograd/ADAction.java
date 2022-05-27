65b460680203f47d30c94681d0a91700661bcc37
Gleethos <Gleethos@gmx.at>
2022-05-27 12:57:27 +0200

    fix missing interface

---
+++ src/main/java/neureka/autograd/ADAction.java
@@ -0,0 +1,22 @@
+package neureka.autograd;
+
+import neureka.Tsr;
+
+/**
+ *  This interface is the declaration for
+ *  lambda actions for both the {@link #act(Target)} method of the {@link ADAgent} interface. <br><br>
+ *  Implementations of this perform auto-differentiation forwards or backwards along the computation graph.
+ *
+ *
+ * Note: Do not access the {@link GraphNode#getPayload()} of the {@link GraphNode}
+ *       passed to implementation of this.
+ *       The payload is weakly referenced, meaning that this method can return null!
+ */
+public interface ADAction
+{
+    /**
+     * @param target A container for the {@link GraphNode} at which the differentiation ought to be performed and error which ought to be used for the forward or backward differentiation.
+     * @return The result of a forward or backward mode auto differentiation.
+     */
+    Tsr<?> act( Target<?> target );
+}
