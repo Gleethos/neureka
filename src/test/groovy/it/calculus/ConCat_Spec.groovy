9eebe4f9c7ce633d7aad8e0d2b0cd1b23cf16315
Gleethos <danneydan@gmx.at>
2022-06-01 12:30:53 +0200

    improve test documentation

--- src/test/groovy/it/calculus/ConCat_Spec.groovy
+++ src/test/groovy/it/calculus/ConCat_Spec.groovy
@@ -1,15 +1,32 @@
 package it.calculus
 
+import neureka.Neureka
 import neureka.Tsr
 import neureka.calculus.Function
 import neureka.calculus.args.Arg
+import neureka.devices.Device
+import neureka.devices.host.CPU
+import neureka.devices.opencl.OpenCLDevice
+import spock.lang.IgnoreIf
+import spock.lang.Narrative
 import spock.lang.Specification
+import spock.lang.Title
 
+@Title("Merging Tensors")
+@Narrative('''
+
+    Tensors can not only be sliced, but also merged.
+    This is most easily achieved through the concatenation operation, 
+    which stacks 2 tensors alongside a specified axis.
+    This specification not only covers how you can concatenate tensors,
+    but also how this works alongside autograd and non-numeric tensors.
+
+''')
 class ConCat_Spec extends Specification
 {
     def 'We can concatenate 2 tensors alongside a specified axis!'()
     {
-        given : 'We create 2 rank 3 tensors, which we want to concatinate.'
+        given : 'We create 2 rank 3 tensors, which we want to concatenate, where the first one requires gradients.'
             var a = Tsr.of(Double, [3, 4, 2], [-1.7, 2, 9.3, -3]).setRqsGradient(true)
             var b = Tsr.ofDoubles().withShape(3, 2, 2).andFill(3,2.5,-6)
         and : 'A function which should perform the concatenation.'
@@ -32,16 +49,16 @@ class ConCat_Spec extends Specification
 
     def 'We can concatenate 2 float tensors alongside a specified axis!'()
     {
-        given :
+        given : 'We create 2 rank 2 tensors, which we want to concatenate, where both require gradients.'
             var a = Tsr.of(Float, [4, 2], [8f, -9f, 5f]).setRqsGradient(true)
             var b = Tsr.ofFloats().withShape(4, 3).andFill(1,6,-6,3).setRqsGradient(true)
-        and :
+        and : 'A function which should perform the concatenation.'
             var cat = Function.of('concat(I[0], I[1])')
 
-        when :
+        when : 'We call the previously created function alongside the axis alongside we want to concatenate.'
             var c = cat.callWith(Arg.Axis.of(1)).call(a, b)
 
-        then :
+        then : 'The resulting tensor should have the expected shape.'
             c.shape() == [4, 5]
 
         when :
@@ -57,35 +74,36 @@ class ConCat_Spec extends Specification
 
     def 'We can concatenate 2 string tensors alongside a specified axis!'()
     {
-        given :
+        given : 'We create 2 rank 2 string tensors, which we want to concatenate, where both require gradients.'
             var a = Tsr.of(String, [2, 5], [':)', ':P', 'B)'])
             var b = Tsr.of(String).withShape(1, 5).andFill('O.o', '._.')
-        and :
+        and : 'A function which should perform the concatenation.'
             var cat = Function.of('concat(I[0], I[1])')
 
-        when :
+        when : 'We call the previously created function alongside the axis alongside we want to concatenate.'
             var c = cat.callWith(Arg.Axis.of(0)).call(a, b)
 
-        then :
+        then : 'The resulting tensor should have the expected shape.'
             c.shape() == [3, 5]
         and :
             c.any( it -> it == ':P' )
             c.any( it -> it == '._.' )
     }
 
-
-    def 'We can concatenate and then back-propagate 2 simple float tensors alongside a specified axis!'()
-    {
-        given :
+    @IgnoreIf({ !Neureka.get().canAccessOpenCLDevice() && data.device == null })
+    def 'We can concatenate and then back-propagate 2 simple float tensors alongside a specified axis!'(
+        Device<?> device
+    ) {
+        given : 'We create 2 rank 2 tensors, which we want to concatenate, where both require gradients.'
             var a = Tsr.of(Float, [3, 1], [8, -4, 7]).setRqsGradient(true)
             var b = Tsr.of(Float).withShape(3, 1).andFill(5, -1, 2).setRqsGradient(true)
-        and :
+        and : 'A function which should perform the concatenation.'
             var cat = Function.of('concat(I[0], I[1])')
 
-        when :
+        when : 'We call the previously created function alongside the axis alongside we want to concatenate.'
             var c = cat.callWith(Arg.Axis.of(1)).call(a, b)
 
-        then :
+        then : 'The resulting tensor should have the expected shape.'
             c.shape() == [3, 2]
 
         when :
@@ -96,6 +114,9 @@ class ConCat_Spec extends Specification
         then :
             a.gradient.value == [-0.5, 0.25, -0.05] as float[]
             b.gradient.value == [1.0, 1.5, 2.0] as float[]
+
+        where :
+            device << [CPU.get(), Device.get(OpenCLDevice, 'gpu')]
     }
 
 
