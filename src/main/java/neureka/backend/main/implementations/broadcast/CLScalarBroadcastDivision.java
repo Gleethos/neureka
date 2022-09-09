28c247931a72dd52084525fc946af17f299b57f0
Gleethos <Gleethos@gmx.at>
2022-09-09 13:39:35 +0200

    refactor/simplify OpenCL device implementation

--- src/main/java/neureka/backend/main/implementations/broadcast/CLScalarBroadcastDivision.java
+++ src/main/java/neureka/backend/main/implementations/broadcast/CLScalarBroadcastDivision.java
@@ -1,42 +1,36 @@
 package neureka.backend.main.implementations.broadcast;
 
-import neureka.backend.main.algorithms.Scalarization;
-import neureka.backend.main.implementations.CLImplementation;
-import neureka.backend.main.implementations.ParsedCLImplementation;
+import neureka.Tsr;
+import neureka.backend.api.ExecutionCall;
 import neureka.calculus.args.Arg;
+import neureka.devices.opencl.OpenCLDevice;
 
-public class CLScalarBroadcastDivision extends ParsedCLImplementation
+public class CLScalarBroadcastDivision extends CLScalarBroadcast
 {
-    public CLScalarBroadcastDivision(String id) {
+    public CLScalarBroadcastDivision( String id ) {
         super(
-            CLImplementation
-            .compiler()
-            .arity( 3 )
-            .kernelSource( Scalarization.getKernelSource() )
-            .activationSource( "output = input1 / value;\n" )
-            .differentiationSource(
+                id,
+                "output = input1 / value;\n",
                 "if (d==0) {                                       \n" +
                 "    output = 1/value;                             \n" +
                 "} else {                                          \n" +
                 "    output = -value /(float)pow(input1, 2.0f);    \n" +
                 "}                                                 \n"
-            )
-            .kernelPostfix( id )
-            .execution(
-                call -> {
-                    int offset = (call.input( Number.class, 2 ).isVirtual() || call.input( Number.class, 2 ).size() == 1)?1:0;
-                    int gwz = call.input( Number.class, 0 ).size();
-                    call.getDevice().getKernel(call)
-                            .passAllOf(call.input( Number.class, 0 ))
-                            .passAllOf(call.input( Number.class, 0 ))
-                            .pass( call.input( Number.class, 1 + offset ).at( 0 ).get().floatValue() )
-                            .pass( call.input( Number.class, 0 ).rank() )
-                            .pass( call.getValOf( Arg.DerivIdx.class ) )
-                            .call( gwz );
-
-                    return call.input( 0 );
-                }
-            )
         );
+    }
+
+    @Override
+    public Tsr<?> run(ExecutionCall<OpenCLDevice> call) {
+        int offset = (call.input( Number.class, 2 ).isVirtual() || call.input( Number.class, 2 ).size() == 1)?1:0;
+        int gwz = call.input( Number.class, 0 ).size();
+        call.getDevice().getKernel(call)
+                .passAllOf(call.input( Number.class, 0 ))
+                .passAllOf(call.input( Number.class, 0 ))
+                .pass( call.input( Number.class, 1 + offset ).at( 0 ).get().floatValue() )
+                .pass( call.input( Number.class, 0 ).rank() )
+                .pass( call.getValOf( Arg.DerivIdx.class ) )
+                .call( gwz );
+
+        return call.input( 0 );
     }
 }
