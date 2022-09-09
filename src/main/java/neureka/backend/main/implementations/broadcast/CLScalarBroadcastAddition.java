22409834c04bd53855caf51b3240d1b29a7060ff
Gleethos <Gleethos@gmx.at>
2022-09-09 13:36:35 +0200

    refactor/simplify OpenCL device implementation

--- src/main/java/neureka/backend/main/implementations/broadcast/CLScalarBroadcastAddition.java
+++ src/main/java/neureka/backend/main/implementations/broadcast/CLScalarBroadcastAddition.java
@@ -1,44 +1,38 @@
 package neureka.backend.main.implementations.broadcast;
 
 import neureka.Tsr;
+import neureka.backend.api.ExecutionCall;
 import neureka.backend.main.algorithms.Scalarization;
 import neureka.backend.main.implementations.CLImplementation;
 import neureka.backend.main.implementations.ParsedCLImplementation;
 import neureka.calculus.args.Arg;
+import neureka.devices.opencl.OpenCLDevice;
 
-public class CLScalarBroadcastAddition extends ParsedCLImplementation
+public class CLScalarBroadcastAddition extends CLScalarBroadcast
 {
     public CLScalarBroadcastAddition(String id) {
-        super(
-            CLImplementation
-            .compiler()
-            .arity( 3 )
-            .kernelSource( Scalarization.getKernelSource() )
-            .activationSource( "output = input1 + value;\n" )
-            .differentiationSource( "output = 1;\n" )
-            .kernelPostfix( id )
-            .execution(
-                call -> {
-                    assert call.arity() == 3;
-                    if ( call.getDerivativeIndex() == 0 )
-                         return Tsr.of( call.input(1).shape(), 1d ).getUnsafe().setIsIntermediate( true );
-                    else if ( call.getDerivativeIndex() == 1 )
-                        return Tsr.of( call.input( 2 ).shape(), 1d ).getUnsafe().setIsIntermediate( true );
-                    else {
-                        int gwz = call.input(Number.class, 0).size();
-                        float value = call.input(Number.class, 2).item(0).floatValue();
-                        call.getDevice()
-                            .getKernel(call)
-                            .passAllOf(call.input(Number.class, 0))
-                            .passAllOf(call.input(Number.class, 1))
-                            .pass(value)
-                            .pass(call.input(Number.class, 0).rank())
-                            .pass(call.getValOf(Arg.DerivIdx.class))
-                            .call(gwz);
-                    }
-                    return call.input( 0 );
-                }
-            )
-        );
+        super( id,  "output = input1 + value;\n", "output = 1;\n" );
+    }
+
+    @Override
+    public Tsr<?> run(ExecutionCall<OpenCLDevice> call) {
+        assert call.arity() == 3;
+        if ( call.getDerivativeIndex() == 0 )
+            return Tsr.of( call.input(1).shape(), 1d ).getUnsafe().setIsIntermediate( true );
+        else if ( call.getDerivativeIndex() == 1 )
+            return Tsr.of( call.input( 2 ).shape(), 1d ).getUnsafe().setIsIntermediate( true );
+        else {
+            int gwz = call.input(Number.class, 0).size();
+            float value = call.input(Number.class, 2).item(0).floatValue();
+            call.getDevice()
+                    .getKernel(call)
+                    .passAllOf(call.input(Number.class, 0))
+                    .passAllOf(call.input(Number.class, 1))
+                    .pass(value)
+                    .pass(call.input(Number.class, 0).rank())
+                    .pass(call.getValOf(Arg.DerivIdx.class))
+                    .call(gwz);
+        }
+        return call.input( 0 );
     }
 }
