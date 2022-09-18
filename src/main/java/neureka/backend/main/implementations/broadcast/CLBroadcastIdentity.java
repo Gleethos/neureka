5c2f44429cee98f0e37847ca6ea6e9a78adbc244
Gleethos <Gleethos@gmx.at>
2022-09-18 12:13:37 +0200

    move further towards backend context based device operation implementation loading

---
+++ src/main/java/neureka/backend/main/implementations/broadcast/CLBroadcastIdentity.java
@@ -0,0 +1,33 @@
+package neureka.backend.main.implementations.broadcast;
+
+import neureka.Tsr;
+import neureka.backend.api.ExecutionCall;
+import neureka.calculus.args.Arg;
+import neureka.devices.opencl.OpenCLDevice;
+
+public class CLBroadcastIdentity extends CLBroadcast
+{
+    public CLBroadcastIdentity(String id) {
+        super(
+             id,
+             "output = value;\n",
+             "output = value;\n"
+        );
+    }
+
+    @Override
+    public Tsr<?> run(ExecutionCall<OpenCLDevice> call) {
+        Tsr<Number> t = call.input( Number.class, 0 );
+        int gwz = t.size();
+        call.getDevice()
+                .getKernel(call)
+                .passAllOf( t )
+                .passAllOf( t )
+                .pass( call.input( Number.class, 1 ).at(0).get().floatValue() )
+                .pass( t.rank() )
+                .pass( call.getValOf( Arg.DerivIdx.class ) )
+                .call( gwz );
+
+        return call.input(0);
+    }
+}
