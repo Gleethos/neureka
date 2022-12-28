55cc181734af03714d0f8438ed2646d980fe0990
Gleethos <Gleethos@gmx.at>
2022-12-28 18:30:56 +0100

    make it possible to instantiate Adam with custom time

--- src/main/java/neureka/optimization/implementations/ADAMFactory.java
+++ src/main/java/neureka/optimization/implementations/ADAMFactory.java
@@ -6,21 +6,27 @@ import neureka.optimization.OptimizerFactory;
 public class ADAMFactory implements OptimizerFactory 
 {
     private final double _learningRate;
+    private final long _time;
 
-    public ADAMFactory() { _learningRate = 0.01; }
+    public ADAMFactory() { this(0.01, 0); }
 
     // The copy constructor should be private, use withers instead!
-    private ADAMFactory(double learningRate) {
+    private ADAMFactory(double learningRate, long time) {
+        if ( time < 0 ) throw new IllegalArgumentException("The time must be a positive number!");
         _learningRate = learningRate;
+        _time = time;
     }
     
     // Withers:
 
     public ADAMFactory withLearningRate(double learningRate) {
-        return new ADAMFactory(learningRate);
+        return new ADAMFactory(learningRate, _time);
     }
 
-    
+    public ADAMFactory withTime(long time) {
+        return new ADAMFactory(_learningRate, time);
+    }
+
     @Override
     public <V extends Number> ADAM<V> create(Tsr<V> target) {
         return new ADAM<>(0, _learningRate, target);
