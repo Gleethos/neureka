8b2e736cba8ac2670d452455ff16f101280e9821
Daniel Nepp <daniel.nepp@drei.at>
2023-04-17 20:06:19 +0200

    fix functions sometimes being parsed inconsistently with respect to the autograd system!

--- src/main/java/neureka/math/parsing/FunctionParser.java
+++ src/main/java/neureka/math/parsing/FunctionParser.java
@@ -229,7 +229,7 @@ public class FunctionParser
         String component = ParseUtil.unpackAndCorrect( foundComponent );
 
         if ( _constantPattern.matcher( component ).matches()   ) return new FunctionConstant( component );
-        else if ( _inputPattern.matcher( component ).find()    ) return FunctionInput.of( component );
+        else if ( _inputPattern.matcher( component ).find()    ) return FunctionInput.of( component, doAD );
         else if ( _variablePattern.matcher( component ).find() ) return new FunctionVariable( component );
         else if ( component.startsWith("-") ) {
             component = "-1 * "+component.substring(1);
