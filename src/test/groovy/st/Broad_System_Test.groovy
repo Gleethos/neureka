ae8fe56ee64ff64ada14ba3ad56d742f0b133cd0
Gleethos <Gleethos@gmx.at>
2023-12-29 18:22:24 +0100

    improve testing

--- src/test/groovy/st/Broad_System_Test.groovy
+++ src/test/groovy/st/Broad_System_Test.groovy
@@ -44,21 +44,52 @@ class Broad_System_Test extends Specification
             tensor2.setRqsGradient(true);
 
         when :
-            Tensor<Double> product = Tensor.of("softplus((I[0]xI[1])*-100)", [tensor1, tensor2]);
-            //String result = product.toString("rc");
+            Tensor<Double> product1 = Tensor.of("softplus((I[0]xI[1])*-100)", [tensor1, tensor2]);
+            Tensor<Double> product2 = (Tensor.of("i0 x i1", tensor1, tensor2)*-100).softplus();
         then :
-            product.toString() == "[2x3]:(200.0, 200.0, 200.0, 200.0, 200.0, 200.0); ->d[2x3]:(-100.0, -100.0, -100.0, -100.0, -100.0, -100.0)"
-        //and :
-            //result.contains("[2x3]:(200.0, 200.0, 200.0, 200.0, 200.0, 200.0);")
-            //result.contains(" =>d|[ [2x3]:(-100.0, -100.0, -100.0, -100.0, -100.0, -100.0) ]|:t{ [2x3]:(-2.0, -2.0, -2.0, -2.0, -2.0, -2.0);")
-            //result.contains(" =>d|[ [1x3]:(2.0, 2.0, 2.0) ]|:t{ [2x1]:(-1.0, -1.0) }")
-            //result.contains(" =>d|[ [2x1]:(-1.0, -1.0) ]|:t{ [1x3]:(2.0, 2.0, 2.0) }")
+            product1.toString() == "[2x3]:(200.0, 200.0, 200.0, 200.0, 200.0, 200.0); ->d[2x3]:(-100.0, -100.0, -100.0, -100.0, -100.0, -100.0)"
+            product2.toString() == "[2x3]:(200.0, 200.0, 200.0, 200.0, 200.0, 200.0); ->d[2x3]:(-100.0, -100.0, -100.0, -100.0, -100.0, -100.0)"
 
         when : 'We perform a backwards pass of a gradient of `-0.1`:'
-            product.backward( -0.1 );
+            product1.backward( -0.1 );
         then :
             tensor1.gradient.get().toString() == "[1x3]:(-20.0, -20.0, -20.0)"
             tensor2.gradient.get().toString() == "[2x1]:(60.0, 60.0)"
+
+        when : 'We perform a backwards pass of a gradient of `-0.1`:'
+            product2.backward( -0.1 );
+        then :
+            tensor1.gradient.get().toString() == "[1x3]:(-40.0, -40.0, -40.0)"
+            tensor2.gradient.get().toString() == "[2x1]:(120.0, 120.0)"
+    }
+
+    def 'A function with expression "softplus(tanh(I[0]*I[1]*2)*I[1])" can be backpropagated.'()
+    {
+        given :
+            Neureka.get().settings().view().getNDPrintSettings().setIsLegacy(true);
+            Tensor<Double> tensor1 = Tensor.of(Shape.of(2), 2d);
+            Tensor<Double> tensor2 = Tensor.of(Shape.of(2), 4d);
+            tensor1.setRqsGradient(true);
+            tensor2.setRqsGradient(true);
+
+        when :
+            Tensor<Double> product1 = Tensor.of("softplus(tanh(I[0]*I[1]*2)*I[1])", [tensor1, tensor2]);
+            Tensor<Double> product2 = ((tensor1 * tensor2 * 2).tanh()*tensor2).softplus();
+        then :
+            product1.toString({it.hasDerivatives=false}) == "[2]:(4.01815, 4.01815)"
+            product2.toString({it.hasDerivatives=false}) == "[2]:(4.01815, 4.01815)"
+
+        when : 'We perform a backwards pass of a gradient of `100`:'
+            product1.backward( 100 );
+        then :
+            tensor1.gradient.get().toString() == "[2]:(159.09e-12, 159.09e-12)"
+            tensor2.gradient.get().toString() == "[2]:(98.2014, 98.2014)"
+
+        when : 'We perform a backwards pass of a gradient of `100`:'
+            product2.backward( 100 );
+        then :
+            tensor1.gradient.get().toString() == "[2]:(318.18e-12, 318.18e-12)"
+            tensor2.gradient.get().toString() == "[2]:(196.403, 196.403)"
     }
 
 }
